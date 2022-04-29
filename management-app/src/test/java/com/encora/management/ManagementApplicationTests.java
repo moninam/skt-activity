74ea5ccbce9efba6a9ae38435df33d20a37d4ce2
moninam <moninambri@gmail.com>
2022-04-29 18:42:50 -0500

    Project Base

---
+++ management-app/src/test/java/com/encora/management/ManagementApplicationTests.java
@@ -0,0 +1,11 @@
+package com.encora.management;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ManagementApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
---
+++ microservice/src/test/java/com/encora/microservice/MicroserviceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.encora.microservice;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MicroserviceApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
