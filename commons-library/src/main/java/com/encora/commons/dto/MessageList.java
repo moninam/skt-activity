74057008554ead28ba34b14525ce11b3c03c6d8b
moninam <moninambri@gmail.com>
2022-05-06 10:16:31 -0500

    Implementation of queue with Rabbitmq

---
+++ commons-library/src/main/java/com/encora/commons/dto/MessageList.java
@@ -0,0 +1,40 @@
+package com.encora.commons.dto;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Component
+@JsonIdentityInfo(generator = ObjectIdGenerators.IntSequenceGenerator.class, property = "@id", scope = MessageList.class)
+public class MessageList implements Serializable {
+    private String tag;
+    private List<Product> productList;
+
+    private MessageList(){
+        this.tag = UUID.randomUUID().toString();
+        productList = new ArrayList<>();
+    }
+
+    public void setList(List<Product> products){
+        this.productList.addAll(products);
+    }
+    public List<Product> getProductList(){
+        return new ArrayList<>(productList);
+    }
+    public String getTag(){
+        return this.tag;
+    }
+
+    @Override
+    public String toString() {
+        return "MessageList{" +
+                "tag='" + tag + '\'' +
+                ", productList=" + productList +
+                '}';
+    }
+}
